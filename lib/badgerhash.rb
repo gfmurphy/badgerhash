24211e279d5fafbaee5525cac62eb4b983cf615f
George F. Murphy <gmurphy@epublishing.com>
2014-03-23 18:50:08 -0500

    Add method that resets class variable values to nil.

--- lib/badgerhash.rb
+++ lib/badgerhash.rb
@@ -2,4 +2,13 @@ require "badgerhash/parsers/nokogiri"
 require "badgerhash/version"
 
 module Badgerhash
+  DEFAULT_SAX_PARSER = Parsers::Nokogiri::SaxDocument
+
+  def self.sax_parser=(parser)
+    @@sax_parser = parser
+  end
+
+  def self.sax_parser
+    @@sax_parser || DEFAULT_SAX_PARSER
+  end
 end
