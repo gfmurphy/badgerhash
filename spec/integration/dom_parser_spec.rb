859f867cafb042a9f0912faacc9678da95a8ea73
George F. Murphy <gmurphy@epublishing.com>
2014-03-30 18:25:04 -0500

    Move shared examples for parser spec into rspec shared example

--- spec/integration/dom_parser_spec.rb
+++ spec/integration/dom_parser_spec.rb
@@ -1,13 +1,7 @@
 require File.expand_path "../../spec_helper.rb", __FILE__
 
 describe "Parsing XML Document", integration: true do
-  subject(:badgerfish) { Badgerhash::XmlDocument.create(xml).to_badgerfish }
-
-  context "with simple document" do
-    let(:xml) { StringIO.new("<alice>bob</alice>") }
-
-    it "parses document correctly" do
-      expect(badgerfish).to eq({"alice" => {"\$" => "bob"}})
-    end
+  it_behaves_like "a badgerfish parser" do
+    subject(:badgerfish) { Badgerhash::XmlDocument.create(xml).to_badgerfish }
   end
 end
